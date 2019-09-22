ea241b158f2d0ce80ebaca5940138388eacaf2fa
sandynz <sandynz@126.com>
2019-09-22 23:15:30 +0800

    validation tests

---
+++ src/test/java/org/sandynz/validation/ValidTestBean.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sandynz.validation;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.sandynz.validation.constraints.CnIdCardNo;
+import org.sandynz.validation.constraints.EqualsAnyInt;
+import org.sandynz.validation.constraints.EqualsAnyString;
+import org.sandynz.validation.constraints.LooseCnMobile;
+import org.sandynz.validation.constraints.LooseCnTel;
+import org.sandynz.validation.constraints.hibernate.LooseCnMobileOrTel;
+
+@Data
+@Accessors(chain = true)
+class ValidTestBean {
+
+    @LooseCnMobile
+    private String cnMobile;
+
+    @LooseCnTel
+    private String cnTel;
+
+    @LooseCnMobileOrTel
+    private String cnMobileOrTel;
+
+    @EqualsAnyInt(value = {1, 2}, message = "Must be one of 1 / 2")
+    private Integer type;
+
+    @EqualsAnyString(value = {"Male", "Female"}, message = "Must be one of Male / Female")
+    private String sex;
+
+    @CnIdCardNo(minAge = 18, maxAge = 65)
+    private String cnIdCardNo;
+
+}
---
+++ src/test/java/org/sandynz/validation/ValidationTest.java
@@ -0,0 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sandynz.validation;
+
+import com.alibaba.fastjson.JSON;
+import java.util.Arrays;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * All validation test cases.
+ *
+ * @author sandynz
+ */
+@Slf4j
+public class ValidationTest {
+
+    private void test0(List<Pair<ValidTestBean, Boolean>> paramResultPairList) {
+        for (Pair<ValidTestBean, Boolean> paramResultPair : paramResultPairList) {
+            ValidTestBean testBean = paramResultPair.getLeft();
+            boolean expectedResult = paramResultPair.getRight();
+            boolean validateResult = Validations.validateBean(testBean);
+            log.info("testBean={}, expectedResult={}, validateResult={}", JSON.toJSONString(testBean), expectedResult, validateResult);
+            Assert.assertEquals(expectedResult, validateResult);
+        }
+    }
+
+    @Test
+    public void testLooseCnMobile() {
+        List<Pair<ValidTestBean, Boolean>> paramResultPairList = Arrays.asList(
+                Pair.of(new ValidTestBean().setCnMobile("12345678"), false),
+                Pair.of(new ValidTestBean().setCnMobile("23012345678"), false),
+                Pair.of(new ValidTestBean().setCnMobile("13012345678"), true)
+        );
+        test0(paramResultPairList);
+    }
+
+    @Test
+    public void testLooseCnTel() {
+        List<Pair<ValidTestBean, Boolean>> paramResultPairList = Arrays.asList(
+                Pair.of(new ValidTestBean().setCnTel("2301234567"), false),
+                Pair.of(new ValidTestBean().setCnTel("010-12345678"), true),
+                Pair.of(new ValidTestBean().setCnTel("0571-87654321"), true)
+        );
+        test0(paramResultPairList);
+    }
+
+    @Test
+    public void testLooseCnMobileOrTel() {
+        List<Pair<ValidTestBean, Boolean>> paramResultPairList = Arrays.asList(
+                Pair.of(new ValidTestBean().setCnMobileOrTel("23012345678"), false),
+                Pair.of(new ValidTestBean().setCnMobileOrTel("010-12345678"), true),
+                Pair.of(new ValidTestBean().setCnMobileOrTel("0571-87654321"), true)
+        );
+        test0(paramResultPairList);
+    }
+
+    @Test
+    public void testEqualsAnyInt() {
+        List<Pair<ValidTestBean, Boolean>> paramResultPairList = Arrays.asList(
+                Pair.of(new ValidTestBean().setType(3), false),
+                Pair.of(new ValidTestBean().setType(1), true)
+        );
+        test0(paramResultPairList);
+    }
+
+    @Test
+    public void testEqualsAnyString() {
+        List<Pair<ValidTestBean, Boolean>> paramResultPairList = Arrays.asList(
+                Pair.of(new ValidTestBean().setSex("F"), false),
+                Pair.of(new ValidTestBean().setSex("Male"), true)
+        );
+        test0(paramResultPairList);
+    }
+
+    @Test
+    public void testCnIdCardNo() {
+        List<Pair<ValidTestBean, Boolean>> paramResultPairList = Arrays.asList(
+                Pair.of(new ValidTestBean().setCnIdCardNo("53010219200508011x"), false),
+                Pair.of(new ValidTestBean().setCnIdCardNo("530102199005080111"), true)
+        );
+        test0(paramResultPairList);
+    }
+
+}
