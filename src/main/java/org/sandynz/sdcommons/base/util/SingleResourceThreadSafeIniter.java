a0bc81db28b5c7817488bac2127467320333ce13
sandynz <sandynz@126.com>
2019-09-28 17:46:21 +0800

    resource initer

---
+++ src/main/java/org/sandynz/sdcommons/base/util/SingleResourceThreadSafeIniter.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sandynz.sdcommons.base.util;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+
+/**
+ * Thread-safe single resource initializer.
+ *
+ * @param <Result> the result type
+ * @param <Input>  the input parameter type
+ * @author sandynz
+ */
+public class SingleResourceThreadSafeIniter<Result, Input> {
+
+    private final AtomicReference<Optional<Result>> resultAtomicReference = new AtomicReference<>();
+
+    public Result initOnceAndGet(Function<Input, Result> resourceFunction, Input input) {
+        return initOnceAndGet(resourceFunction, input, false);
+    }
+
+    @SuppressWarnings("OptionalAssignedToNull")
+    public Result initOnceAndGet(Function<Input, Result> resourceFunction, Input input, boolean cacheNullResult) {
+        Optional<Result> optionalResult = resultAtomicReference.get();
+        if (optionalResult == null) {
+            synchronized (resultAtomicReference) {
+                optionalResult = resultAtomicReference.get();
+                if (optionalResult == null) {
+                    Result result = resourceFunction.apply(input);
+                    if (result != null || cacheNullResult) {
+                        optionalResult = Optional.ofNullable(result);
+                        resultAtomicReference.set(optionalResult);
+                        return optionalResult.orElse(null);
+                    } else {
+                        return null;
+                    }
+                } else {
+                    return optionalResult.orElse(null);
+                }
+            }
+        } else {
+            return optionalResult.orElse(null);
+        }
+    }
+
+}
---
+++ src/test/java/org/sandynz/sdcommons/base/util/SingleResourceThreadSafeIniterTest.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sandynz.sdcommons.base.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * {@link SingleResourceThreadSafeIniter} test cases.
+ *
+ * @author sandynz
+ */
+@Slf4j
+public class SingleResourceThreadSafeIniterTest {
+
+    @Data
+    @Accessors(chain = true)
+    private static class ExecutorServiceCfg {
+
+        private int corePoolSize;
+        private int maxPoolSize;
+        private int queryCapacity;
+
+    }
+
+    @Test
+    public void testInitOnce() {
+        ExecutorServiceCfg cfg = new ExecutorServiceCfg().setCorePoolSize(1).setMaxPoolSize(10).setQueryCapacity(50);
+        SingleResourceThreadSafeIniter<ExecutorService, ExecutorServiceCfg> initer = new SingleResourceThreadSafeIniter<>();
+        AtomicReference<Boolean> inited = new AtomicReference<>(false);
+        List<ExecutorService> executorServiceList = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            ExecutorService executorService = initer.initOnceAndGet(executorServiceCfg -> {
+                if (!inited.compareAndSet(false, true)) {
+                    throw new RuntimeException("inited not only once");
+                }
+                return new ThreadPoolExecutor(cfg.corePoolSize, cfg.maxPoolSize, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(cfg.getQueryCapacity()));
+            }, cfg);
+            executorServiceList.add(executorService);
+        }
+        Assert.assertTrue(executorServiceList.size() > 0);
+        Iterator<ExecutorService> iterator = executorServiceList.iterator();
+        ExecutorService executorService = iterator.next();
+        log.info("executorService={}", executorService);
+        while (iterator.hasNext()) {
+            Assert.assertSame(executorService, iterator.next());
+        }
+    }
+
+    private void testCacheNullResult0(boolean cacheNullResult) {
+        SingleResourceThreadSafeIniter<String, Void> initer = new SingleResourceThreadSafeIniter<>();
+        AtomicReference<Boolean> inited = new AtomicReference<>(false);
+        List<String> stringList = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            String result = initer.initOnceAndGet(aVoid -> {
+                if (!inited.compareAndSet(false, true)) {
+                    throw new RuntimeException("inited not only once");
+                }
+                return null;
+            }, null, cacheNullResult);
+            stringList.add(result);
+        }
+        for (String result : stringList) {
+            Assert.assertNull(result);
+        }
+    }
+
+    @Test
+    public void testCacheNullResult1() {
+        testCacheNullResult0(true);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testCacheNullResult2() {
+        testCacheNullResult0(false);
+    }
+
+}
